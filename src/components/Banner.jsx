2948425cc7538e51b6d7cebca0b745ad8b20112e
OMAR26275 <omarosama005005@gmail.com>
2025-11-20 20:43:58 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img
+        src="/images/banner.jpg"
+        alt="Udemy Banner"
+        className="banner-img"
+      />
+    </div>
+  );
+};
+
+export default Banner;
