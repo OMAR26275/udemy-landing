6cbe9ad81e3296ec1c927e427d5b5d243f6b27f9
OMAR26275 <omarosama005005@gmail.com>
2025-11-20 20:44:34 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,23 @@
+import React from "react";
 
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-search">
+        <div className="logo">Udemy</div>
+        <input
+          type="text"
+          placeholder="Search for anything"
+          className="search-bar"
+        />
+      </div>
+      <div className="header-actions">
+        <span className="icon">🛒</span>
+        <button className="signup-btn"> Login</button>
+        <button className="signup-btn">Sign Up</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
