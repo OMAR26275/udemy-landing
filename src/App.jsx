76728525ffdf5395fb79a23b67c8ad80011a862e
OMAR26275 <omarosama005005@gmail.com>
2025-11-20 20:46:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import HeroSection from "./components/HeroSection";
+import CoursesSection from "./components/CoursesSection";
+import TrustedCompanies from "./components/TrustedCompanies";
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <HeroSection />
+      <CoursesSection />
+      <TrustedCompanies />
+    </div>
+  );
+}
+
+export default App;
